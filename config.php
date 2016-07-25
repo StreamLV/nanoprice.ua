a6a556bbbb2931650a17538b4d2b1c217d39f979
stream <l.vitaliy.m@gmail.com>
2016-07-25 22:38:41 +0300

    111

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://127.0.0.1/nanoprice.ua/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://127.0.0.1/nanoprice.ua/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/nanoprice.ua/catalog/');
+define('DIR_SYSTEM', '/var/www/html/nanoprice.ua/system/');
+define('DIR_LANGUAGE', '/var/www/html/nanoprice.ua/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/html/nanoprice.ua/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/html/nanoprice.ua/system/config/');
+define('DIR_IMAGE', '/var/www/html/nanoprice.ua/image/');
+define('DIR_CACHE', '/var/www/html/nanoprice.ua/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/nanoprice.ua/system/storage/download/');
+define('DIR_LOGS', '/var/www/html/nanoprice.ua/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/html/nanoprice.ua/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/html/nanoprice.ua/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '3plus39');
+define('DB_DATABASE', 'nanoprice');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'np_');
